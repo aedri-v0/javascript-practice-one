004d50abec38aaa91e48e391d36960294e0032e4
aedri-v0 <aedri-v0@users.noreply.github.com>
2021-11-04 23:29:45 +0000

    deploy: f0eb68f7e11b9b0a4088445424c971a31482e010

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
+let first = "Adrian";
+let last = "Tillman";
 
+console.log(first, last);
+var button = document.querySelector("button");
+
+function logName() {
+  console.log(first, last);
+}
+
+button.addEventListener("click", logName);
+
+didAppend = false;
+
+function appendName() {
+  if (didAppend) return;
+  //create element
+  let p = document.createElement("p");
+  //put text inside element
+  p.innerText = first + " " + last;
+  //append p as a child of body
+  document.body.appendChild(p);
+  didAppend = true;
+}
+
+button.addEventListener("click", appendName);
+
+let hobbies = ["Brawlhalla", "Rapping", "Going Out"];
+let ul = document.createElement("ul");
+
+hobbies.forEach((hobby) => {
+  console.log(hobby);
+  let li = document.createElement("li");
+  li.innerText = hobby;
+  ul.appendChild(li);
+});
+
+document.body.appendChild(ul);
